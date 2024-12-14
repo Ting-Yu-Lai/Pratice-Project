243a33c7ac835460b51d0352e9666775f0123d86
Ting-Yu-Lai <manjusaka1110@gmail.com>
2024-12-14 14:12:09 +0800

    first commit

---
+++ 06_04.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int customers, total = 0;
+
+    printf("Please enter the number of customers :");
+    scanf("%d",&customers);
+
+    if (customers >= 10){
+        total = customers * 300 * 0.8;
+        printf("Total = %d",total);
+    }
+    else{
+        total = customers * 300;
+        printf("Total = %d",total);
+    }
+        
+}
