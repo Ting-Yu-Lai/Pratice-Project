fb33d9f7030c9b7e876348ca05ce2ce0f20e2b43
Ting-Yu-Lai <manjusaka1110@gmail.com>
2024-12-15 11:07:23 +0800

    06_04.c file

--- 06_04.c
+++ 06_04.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 int main()
@@ -6,15 +5,13 @@ int main()
     int customers, total = 0;
 
     printf("Please enter the number of customers :");
-    scanf("%d",&customers);
+    scanf("%d", &customers);
 
-    if (customers >= 10){
+    if (customers >= 10) {
         total = customers * 300 * 0.8;
-        printf("Total = %d",total);
-    }
-    else{
+        printf("Total = %d", total);
+    } else {
         total = customers * 300;
-        printf("Total = %d",total);
+        printf("Total = %d", total);
     }
-        
 }
