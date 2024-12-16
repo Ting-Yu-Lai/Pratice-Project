82ea92c60b8db8e5c2bde7df25c3ace231fe0397
Ting-Yu-Lai <manjusaka1110@gmail.com>
2024-12-16 17:03:09 +0800

    update

---
+++ if_pratice_06/06_07.c
@@ -0,0 +1,25 @@
+//輸入某三角形的三邊長(皆為整數)，判斷這三角形是不是直角三角形
+#include <stdio.h>
+
+int main()
+{
+    int side1 = 0, side2 = 0, side3 = 0;
+    int square_side1 = 0, square_side2 = 0, square_side3 = 0;
+    int square_side12 = 0, square_side23 = 0, square_side13 = 0;
+    
+    printf("Please enter three of lengths :");
+    scanf("%d%d%d", &side1, &side2, &side3);
+
+    square_side1 = side1 * side1;
+    square_side2 = side2 * side2;
+    square_side3 = side3 * side3;
+    square_side12 = square_side1 + square_side2;
+    square_side23 = square_side2 + square_side3;
+    square_side13 = square_side1 + square_side3;
+
+    if (square_side12 >= square_side3 || square_side23 >= square_side1 || square_side13 >= square_side3) 
+    {
+        printf("Right triangle");
+    }
+    return 0;
+}
