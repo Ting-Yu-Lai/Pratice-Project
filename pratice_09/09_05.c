5b22f752424497c5d22b0f1623431de1af72d003
Ting-Yu-Lai <manjusaka1110@gmail.com>
2025-01-07 20:31:50 +0800

    update

---
+++ pratice_09/09_05.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 0;
+    float sum = 0;
+    float average = 0;
+    int count = 1;
+    printf("Enter of numbers(quit with 0) : ");
+    scanf("%d", &num1);
+    while(num1 != 0) {
+        count = count + 1;
+        sum = sum + num1;
+        printf("Enter of numbers(quit with 0) : ");
+        scanf("%d", &num1);
+    }
+    if (num1 == 0){
+        count = count - 1;
+        average = sum / count;
+    }
+    printf("The average is : %.2f", average);
+    return 0;
+}
