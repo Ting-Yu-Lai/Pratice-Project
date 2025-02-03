3b62e5bcd1f37f576fd900c0169498ff2c35f8e8
Ting-Yu-Lai <manjusaka1110@gmail.com>
2025-02-03 18:31:08 +0800

    Update

--- function_13/13_08.c
+++ function_13/13_08.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 int main() {
+    srand(time(0)); //在rand使用之前先用srand(1)來設定隨機數的起始值
     int i;
     for (i = 0; i <= 5; i++){
         printf("%d\n",rand());
